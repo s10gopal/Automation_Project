5fa21c5fa8c3fb62526bac790b7cb398da323ec9
s10gopal <36487439+s10gopal@users.noreply.github.com>
2021-05-16 06:42:41 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -10,5 +10,25 @@ timestamp=$(date '+%d%m%Y-%H%M%S')
 file=$myname-httpd-logs-$timestamp.tar
 
 cd /var/log/apache2/; tar cvf /tmp/$file *.log
-
 aws s3 cp /tmp/$file s3://${s3_bucket}/$file
+
+
+if [[ ! -f /var/www/html/inventory.html ]]
+then
+if [[ ! -d /var/www/html ]]
+then
+mkdir -p /var/www/html
+fi
+echo "Log Type    Time Created    Type    Size" > /var/www/html/inventory.html
+fi
+
+logType="httpd-logs"
+type=${file#*.}
+size=$(du -h /tmp/$file)
+
+echo "$logType		$timestamp		$type		$size" >> /var/www/html/inventory.html
+
+if [[ ! -f /etc/cron.d/automation ]]
+then
+echo "0 0 * * * root /root/Automation_Project/automation.sh" > /etc/cron.d/automation
+fi
